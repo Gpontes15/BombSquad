1abd4e168dcbc852c74c75fb13e2e8c72dcd7e43
PedroGaya <pedrohenriquegaya@gmail.com>
2024-05-17 11:39:03 -0300

    basic game board code

--- data/DifficultySettings.gd
+++ data/DifficultySettings.gd
@@ -4,7 +4,6 @@ class_name DifficultySettings
 
 @export var name: String
 @export var board_size: int
-@export var words: Array[String]
 @export var total_time_sec: int
 @export var lives: int
 @export var hints: int
---
+++ scenes/gameboard/game_board.gd
@@ -0,0 +1,25 @@
+extends Control
+
+@onready var settings: DifficultySettings = GameController.selected_mode
+@onready var grid: GridContainer = %Grid
+
+# [{ text: String, category: String }]
+var selected_words: Array[Dictionary]
+
+func _ready():
+	_select_words()
+	grid.columns = settings.board_size
+
+func _select_words():
+	var word_list: Dictionary = GameController.word_list.duplicate(true)
+	var categories = word_list.keys()
+	categories.shuffle()
+	
+	var selected_categories = categories.slice(0, settings.board_size)
+	
+	for category in selected_categories:
+		var words_in_category = word_list[category]
+		words_in_category.shuffle()
+		var selected_words_by_category = words_in_category.slice(0, settings.board_size)
+		for word in selected_words_by_category:
+			selected_words.append({ "text": word, "category": category })
